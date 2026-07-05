37c44611c95c306ab7203ac86f1115e46583b933
peemchaninunkarn-ux <peemchaninunkarn@gmail.com>
2026-07-05 09:20:44 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,3 @@
-// GPS Tracking - realtime friendly service worker
-// ไม่ cache ข้อมูล Firebase/แผนที่ เพื่อไม่ให้ตำแหน่งค้าง
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request, {cache:'no-store'})); });
